0ab9699bcb297d8d10b547892bfacee344b54856
Christoph Strasen <christoph.strasen.germany@gmail.com>
2018-06-12 17:08:45 +0000

    listening to WASD and logging in dashboard-console. Styling sucks bad.

--- client/test.js
+++
@@ -1,8 +0,0 @@
-var foo = "bar";
-console.log(foo);
-function blubb() {
-  var foo2 = "buff";
-  return foo2;
-}
-
-console.log(blubb());
--- client/web/js/main.js
+++ client/web/js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 /* eslint-disable */
 function initScaledrone() {
   var drone = new Scaledrone("evz2wEE7dGgA4cQ7");
@@ -36,3 +37,61 @@ function initScaledrone() {
     console.error(error);
   });
 }
+
+class Command {
+  constructor(strCommand, destination = "vehicle") {
+    this.strCommand = strCommand;
+    this.destination = destination;
+  }
+  toJSON() {
+    let { strCommand, destination } = this;
+    console.log({ strCommand, destination });
+    return { strCommand, destination };
+  }
+  send() {
+    logToConsoleIn(JSON.stringify(this));
+    return null;
+  }
+}
+
+function logToConsoleIn(str) {
+  var node = document.createElement("div");
+  var textnode = document.createTextNode(str);
+  node.appendChild(textnode);
+  document.getElementById("console").appendChild(node);
+}
+
+// global event listeners
+window.addEventListener(
+  "keydown",
+  function(event) {
+    switch (event.code) {
+      case "KeyW": {
+        let cmd = new Command("pitch_up");
+        cmd.send();
+        break;
+      }
+      case "KeyS": {
+        let cmd = new Command("pitch_down");
+        cmd.send();
+        break;
+      }
+      case "KeyA": {
+        let cmd = new Command("yaw_left");
+        cmd.send();
+        break;
+      }
+      case "KeyD": {
+        let cmd = new Command("yaw_right");
+        cmd.send();
+        break;
+      }
+      case "Space": {
+        let cmd = new Command("position_neutral");
+        cmd.send();
+        break;
+      }
+    }
+  },
+  true
+);
