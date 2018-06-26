64e9746e77efb062681a169da59c3ae68383d9b1
Christoph Strasen <christoph.strasen.germany@gmail.com>
2018-06-26 17:15:04 +0000

    parsing incoming vehicleStats, updating stats-box. Not pretty but works

--- client/web/js/main.js
+++ client/web/js/main.js
@@ -1,32 +1,29 @@
 "use strict";
+var drone = initScaledrone();
+
 /* eslint-disable */
 function initScaledrone() {
-  var drone = new Scaledrone("evz2wEE7dGgA4cQ7");
-  
+  var drone = new Scaledrone("AVMWTdaXnSW1UdUV"); //miwy dev or prod channel 
   /* eslint-enable */
-
   drone.on("open", function(error) {
     if (error) {
       console.error(error);
       return;
     }
-    var room = drone.subscribe("my-room");
-    room.on("open", function(error) {
+    var downstream = drone.subscribe("downstream");
+    downstream.on("open", function(error) {
       if (error) {
         console.error(error);
+        logToConsole(error, "out");
       } else {
-        console.log("Connected to room");
+        console.log("Connected to room downstream");
+        logToConsole("Connected to room downstream","in");
       }
     });
-    room.on("data", function(data) {
+    downstream.on("data", function(data) {
       console.log("Received data:", data);
-    });
-    drone.publish({
-      room: "my-room",
-      message: {
-        name: "Bob",
-        age: 42
-      }
+      logToConsole("Received data:" + JSON.stringify(data), "in");
+      parseDownstream(data);
     });
   });
 
@@ -37,6 +34,26 @@ function initScaledrone() {
   drone.on("error", function(error) {
     console.error(error);
   });
+
+  return drone;
+}
+
+function parseDownstream(data) {
+  if(data.hasOwnProperty("vehicleStats")) {
+    var vStatsIn = new vehicleStats();
+    vStatsIn.fromJSON(data.vehicleStats);
+    logToConsole("found vehicleStats" + JSON.stringify(vStatsIn),"in");
+    updateVehicleStatsBox(vStatsIn, "actual");
+  } 
+}
+
+function updateVehicleStatsBox(obj, type) {
+  var tbody = document.getElementById('telemetry');
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+  var textnode = document.createTextNode(JSON.stringify(obj).replace(/\,/gi,",\n"));
+  tbody.appendChild(textnode);
 }
 
 class Command {
@@ -51,6 +68,10 @@ class Command {
   }
   send() {
     logToConsole(JSON.stringify(this),"out");
+    drone.publish({
+      room: "upstream",
+      message: this
+    });
     return null;
   }
 }
@@ -64,18 +85,48 @@ function logToConsole(str, inout) {
   node.scrollIntoView();
 }
 
+class vehicleStats {
+  constructor() {
+    this.position_neutral();
+  }
+
+  position_neutral() {
+    this.throttle = 0;
+    this.thrust_vector_horizontal = 0;
+    this.thrust_vector_vertical = 0;
+    this.motor_left_direction = 1; //forward
+    this.motor_left_direction = 1; //forward
+    this.center_of_mass_slider_position = 0; //all the way down
+  }
+
+  fromJSON(data) {
+    Object.assign(this, data);
+  }
+
+  toJSON() {
+    let {
+      throttle, thrust_vector_horizontal, thrust_vector_vertical,
+      motor_left_direction, motor_right_direction, center_of_mass_slider_position
+    } = this;
+    return { 
+      throttle, thrust_vector_horizontal, thrust_vector_vertical,
+      motor_left_direction, motor_right_direction, center_of_mass_slider_position
+    };
+  }
+}
+
 // global event listeners
 window.addEventListener(
   "keydown",
   function(event) {
     switch (event.code) {
       case "KeyW": {
-        let cmd = new Command("pitch_up");
+        let cmd = new Command("throttle_up");
         cmd.send();
         break;
       }
       case "KeyS": {
-        let cmd = new Command("pitch_down");
+        let cmd = new Command("throttle_down");
         cmd.send();
         break;
       }
@@ -93,6 +144,19 @@ window.addEventListener(
         let cmd = new Command("position_neutral");
         cmd.send();
         break;
+      }
+      case "KeyL": {
+        var simStats = new vehicleStats();
+        simStats.throttle = 50;
+        drone.publish({
+          room: "downstream",
+          message: {
+            category: "telemetryUpdate",
+            level: "info",
+            note: "client-side injected for development",
+            vehicleStats: simStats
+          }
+        });
       }
     }
   },
