0b5bb036ac917e569b59c49329e9c0fb69c13c1c
Christoph Strasen <christoph.strasen@medigo.com>
2018-07-04 16:01:56 +0000

    fixed remaining console output, restricted layout for long log lines, fonts responsive@

--- client/web/js/main.js
+++ client/web/js/main.js
@@ -6,57 +6,54 @@ function initScaledrone() {
   var drone = new Scaledrone('AVMWTdaXnSW1UdUV') //miwy dev or prod channel 
   drone.on('open', function(error) {
     if (error) {
-      Console.log(error)
+      logToConsole(error)
       return
     }
-    var downstream = drone.subscribe("downstream")
-    downstream.on("open", function(error) {
+    var downstream = drone.subscribe('downstream')
+    downstream.on('open', function(error) {
       if (error) {
-        console.error(error)
-        logToConsole(error, "out")
+        logToConsole(error, 'out')
       } else {
-        console.log("Connected to room downstream")
-        logToConsole("Connected to room downstream","in")
+        logToConsole('Connected to room downstream','in')
       }
-    });
-    downstream.on("data", function(data) {
-      console.log("Received data:", data)
-      logToConsole("Received data:" + JSON.stringify(data), "in")
+    })
+    downstream.on('data', function(data) {
+      logToConsole('Received data:' + JSON.stringify(data), 'in')
       parseDownstream(data)
-    });
-  });
+    })
+  })
 
-  drone.on("close", function(event) {
-    console.log("Connection was closed", event)
-  });
+  drone.on('close', function(event) {
+    logToConsole('Connection was closed', event)
+  })
 
-  drone.on("error", function(error) {
-    console.error(error)
-  });
+  drone.on('error', function(error) {
+    logToConsole(error)
+  })
 
   return drone
 }
 
 function parseDownstream(data) {
-  if(data.hasOwnProperty("vehicleStats")) {
+  if(data.hasOwnProperty('vehicleStats')) {
     var vStatsIn = new vehicleStats()
     vStatsIn.fromJSON(data.vehicleStats)
-    logToConsole("found vehicleStats" + JSON.stringify(vStatsIn),"in")
-    updateVehicleStatsBox(vStatsIn, "actual")
+    logToConsole('found vehicleStats' + JSON.stringify(vStatsIn),'in')
+    updateVehicleStatsBox(vStatsIn, 'actual')
   } 
 }
 
-function updateVehicleStatsBox(obj, type) {
+function updateVehicleStatsBox(obj) {
   var tbody = document.getElementById('telemetry')
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild)
   }
-  var textnode = document.createTextNode(JSON.stringify(obj).replace(/\,/gi,",\n"))
+  var textnode = document.createTextNode(JSON.stringify(obj).replace(/,/gi,',\n'))
   tbody.appendChild(textnode)
 }
 
 class Command {
-  constructor(strCommand, destination = "vehicle") {
+  constructor(strCommand, destination = 'vehicle') {
     this.strCommand = strCommand
     this.destination = destination
     this.createTime = new Date()
@@ -66,11 +63,11 @@ class Command {
     return { createTime, strCommand, destination }
   }
   send() {
-    logToConsole(JSON.stringify(this),"out")
+    logToConsole(JSON.stringify(this),'out')
     drone.publish({
-      room: "upstream",
+      room: 'upstream',
       message: this
-    });
+    })
     return null
   }
 }
@@ -78,14 +75,13 @@ class Command {
 
 
 function logToConsole(str, inout) {
-  var node = document.createElement("li")
+  var node = document.createElement('li')
   var textnode = document.createTextNode(str)
-  var console = document.getElementById("console" + inout)
+  var console = document.getElementById('console' + inout)
   node.appendChild(textnode)
   console.appendChild(node)
   node.scrollIntoView()
 }
-
 class vehicleStats {
   constructor() {
     this.position_neutral()
@@ -118,47 +114,47 @@ class vehicleStats {
 
 // global event listeners
 window.addEventListener(
-  "keydown",
+  'keydown',
   function(event) {
     switch (event.code) {
-      case "KeyW": {
-        let cmd = new Command("throttle_up")
-        cmd.send()
-        break
-      }
-      case "KeyS": {
-        let cmd = new Command("throttle_down")
-        cmd.send()
-        break
-      }
-      case "KeyA": {
-        let cmd = new Command("yaw_left")
-        cmd.send()
-        break
-      }
-      case "KeyD": {
-        let cmd = new Command("yaw_right")
-        cmd.send()
-        break
-      }
-      case "Space": {
-        let cmd = new Command("position_neutral")
-        cmd.send()
-        break
-      }
-      case "KeyL": {
-        var simStats = new vehicleStats()
-        simStats.throttle = 50
-        drone.publish({
-          room: "downstream",
-          message: {
-            category: "telemetryUpdate",
-            level: "info",
-            note: "client-side injected for development",
-            vehicleStats: simStats
-          }
-        })
-      }
+    case 'KeyW': {
+      let cmd = new Command('throttle_up')
+      cmd.send()
+      break
+    }
+    case 'KeyS': {
+      let cmd = new Command('throttle_down')
+      cmd.send()
+      break
+    }
+    case 'KeyA': {
+      let cmd = new Command('yaw_left')
+      cmd.send()
+      break
+    }
+    case 'KeyD': {
+      let cmd = new Command('yaw_right')
+      cmd.send()
+      break
+    }
+    case 'Space': {
+      let cmd = new Command('position_neutral')
+      cmd.send()
+      break
+    }
+    case 'KeyL': {
+      var simStats = new vehicleStats()
+      simStats.throttle = 50
+      drone.publish({
+        room: 'downstream',
+        message: {
+          category: 'telemetryUpdate',
+          level: 'info',
+          note: 'client-side injected for development',
+          vehicleStats: simStats
+        }
+      })
+    }
     }
   },
   true
